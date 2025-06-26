55178ce502e000d86d35853850276a35aebaff40
Christian Coder <203032652+christian-coder-org@users.noreply.github.com>
2025-06-26 16:37:45 -0600

    assets icon and splash screen

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,22 @@
 
 <script setup lang="ts">
 import { IonApp, IonRouterOutlet } from "@ionic/vue";
+//import { SplashScreen } from "@capacitor/splash-screen";
+
+// Show the splash for two seconds and then automatically hide it:
+
+// Hide the splash (you should do this on app launch)
+//await SplashScreen.hide();
+
+// Show the splash for an indefinite amount of time:
+/*await SplashScreen.show({
+  autoHide: false,
+});*/
+
+/*await SplashScreen.show({
+  showDuration: 2000,
+  autoHide: true,
+});*/
 </script>
 
 <style lang="css">
