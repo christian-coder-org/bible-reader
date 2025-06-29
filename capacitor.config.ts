660b5ffc19bd09f8c138a6dbb410d62fd0425a22
Christian Coder <203032652+christian-coder-org@users.noreply.github.com>
2025-06-29 17:02:23 -0600

    last before new repo (#1)

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: "org.christiancoder.biblereader",
+  appId: "com.wyomingsoftware.biblereader",
   appName: "Bible Reader",
   webDir: "dist",
 };
